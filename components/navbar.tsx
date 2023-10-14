0835b4a74f1708ff6cf21e9639e04337d81905dc
vincent edeh <vincentedeh42@gmail.com>
2023-10-14 19:14:45 +0100

    feat: make search component functional

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,7 @@ import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 
 const font = Poppins({
@@ -16,9 +17,10 @@ const font = Poppins({
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items">
-                <Menu className="block md:hidden" />
+                {/* <Menu className="block md:hidden" /> */}
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl", font.className)}>companion.ai</h1>
                 </Link>
